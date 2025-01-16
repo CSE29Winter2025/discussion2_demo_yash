d5173f601bcd7bc6641241e5270e2aee3f364249
Yash Garde <ygarde@ucsd.edu>
2025-01-15 16:36:53 -0800

    fixed add_one

--- add_one.c
+++ add_one.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+// A) 5
+// B) 6
+// C) crash
+// D) a
 
-void add_one(int a) {
-    a += 1;
+void add_one(int *a) {
+    *a += 1;
     return;
 }
 
 int main() {
     int num1 = 5;
-    add_one(num1);
+    add_one(&num1);
     printf("num1: %d\n", num1);
     return 0;
 }
